c96a0f4a47495845bb0559a20454a5b289c807bf
LeoSL <leosilvalima@gmail.com>
2017-12-28 21:32:07 -0500

    Implements card rendering
    - Isolate in 'component-like' structure* the Card, Card Style and Deck abstractions
    - Creates elements and appends them after in DOM
    
    * component-like structure build in a single-file to avoid Babel installation - it is a overhead for this simple app

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+/* global document */
+
+// SHUFFLE FUNCTION //
+
 /*
  * Create a list that holds all of your cards
  */
@@ -11,28 +15,129 @@
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+function shuffle(array) { // eslint-disable-line no-unused-vars
+  const shuffledArray = array;
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    shuffledArray[currentIndex] = shuffledArray[randomIndex];
+    shuffledArray[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return shuffledArray;
 }
 
+//
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// MATH UTILS COMPONENT //
+
+const MathUtils = class MathUtils {
+  static getRandomInt(minNumber, maxNumber) {
+    const min = Math.ceil(minNumber);
+    const max = Math.floor(maxNumber);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  static getRandomCardId() {
+    return Math.round(Math.random() * 10000);
+  }
+};
+
+//
+
+// CARD STYLE LIST COMPONENT //
+
+const CardStylelist = [
+  'fa-diamond',
+  'fa-paper-plane-o',
+  'fa-anchor',
+  'fa-bolt',
+  'fa-cube',
+  'fa-leaf',
+  'fa-bicycle',
+  'fa-bomb',
+];
+
+//
+
+// CARD COMPONENT //
+
+const Card = class Card {
+  constructor(cardId, cardStyle, cardStyleId) {
+    this.cardId = cardId || MathUtils.getRandomCardId();
+    this.cardStyle = cardStyle || CardStylelist[cardStyleId];
+  }
+
+  render() {
+    const card = document.createElement('li');
+    const cardItem = document.createElement('i');
+
+    card.setAttribute('class', 'card');
+    cardItem.setAttribute('id', this.cardId);
+    cardItem.setAttribute('class', `fa ${this.cardStyle}`);
+
+    card.append(cardItem);
+
+    return card;
+  }
+};
+
+//
+
+// DECK COMPONENT //
+
+const Deck = class Deck {
+  constructor(dimension) {
+    this.deckWidth = dimension || 4;
+    this.cardDeck = [];
+  }
+
+  duplicateCards() {
+    let duplicatedCard;
+
+    this.cardDeck.forEach((card) => {
+      duplicatedCard = new Card(card.cardId, card.cardStyle, null);
+      this.cardDeck.push(duplicatedCard);
+    });
+  }
+
+  buildDeck() {
+    const numberOfCards = this.deckWidth * this.deckWidth;
+    let card;
+    let iterator = 0;
+
+    while (iterator < (numberOfCards / 2)) {
+      card = new Card(null, null, iterator);
+      this.cardDeck.push(card);
+      iterator += 1;
+    }
+
+    this.duplicateCards();
+    this.cardDeck = shuffle(this.cardDeck);
+  }
+
+  render() {
+    this.cardDeck.forEach((card) => {
+      document.getElementById('card-deck').append(card.render());
+    });
+  }
+};
+
+//
+
+// MAIN APP COMPONENT //
+
+const App = {
+  init: function init() {
+    const deck = new Deck();
+    deck.buildDeck();
+    deck.render();
+  },
+};
+
+App.init();
